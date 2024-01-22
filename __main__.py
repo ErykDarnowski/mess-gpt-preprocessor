1ab1708ce2b2ffcb7a14a6815592a3dbcf9db73b
ErykDarnowski <github@erykdarnowski.pl>
2024-01-22 21:16:05 +0100

    Implement remove parts

--- __main__.py
+++ __main__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 __author__ = "Eryk Darnowski"
 __version__ = "0.0.0"
 __license__ = "MIT"
@@ -11,6 +13,9 @@ def main():
     with open(input_filename, "r", encoding="utf-8") as input_file:
         transcript = input_file.read()
 
+    # perform cleanup
+    ## remove parts
+    transcript = re.sub(r"^{[0-9]+}\s", "", transcript, 0, re.MULTILINE)
 
 if __name__ == "__main__":
     main()
